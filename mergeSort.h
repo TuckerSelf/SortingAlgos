defc9aa443e3b4bbcd29214257676882223d84e3
Tucker Self <tself3@ut.utm.edu>
2024-09-25 18:38:51 -0500

    SelectionSort

---
+++ mergeSort.h
@@ -0,0 +1,13 @@
+#ifdef MERGESORT_H
+#define MERGESORT_H
+
+#include <vector>
+
+class mergeSort{
+    private:
+
+    public:
+        void mergeSort(std::vector<int> &ar);
+}
+
+#endif //MERGESORT_H
