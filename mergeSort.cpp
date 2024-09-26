84b201431d2b5cb3e42768485fedb7d1f622fc20
Tucker Self <tself3@ut.utm.edu>
2024-09-25 19:28:09 -0500

    Quick Fixes

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -17,15 +17,15 @@ vector<int> separateVector(vector<int> &ar, int startVal, int endVal){
 }
 
 void mergeSort::mergeSort(vector<int> &ar){
-    int frontVal = ar.front;
-    int backVal = ar.back;
+    int frontVal = ar.front();
+    int backVal = ar.back();
     if(frontVal < backVal){
         int midVal = (frontVal + backVal)/2;
         vector<int> frontHalf = separateVector(ar, frontVal, midVal);
         vector<int> backHalf = separateVector(ar, midVal, backVal);
         mergeSort(frontHalf);
         mergeSort(backHalf);
-        int length = backHalf.size;
+        int length = backHalf.size();
         for(int i = 0; i < length; i++){
             frontHalf.push_back(backHalf[i]);
         }
--- selectionSort.cpp
+++ selectionSort.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 void selectionSort::selectionSortA(vector<int> &ar){
-    length = ar.size;
+    int length = ar.size();
     if(length >= 1){
         for(int i = 0; i < length; i++){
             for(int j = i++; j < length; j++){
@@ -19,7 +19,7 @@ void selectionSort::selectionSortA(vector<int> &ar){
 }
 
 void selectionSort::selectionSortB(vector<int> &ar){
-    length = ar.size;
+    int length = ar.size();
     if(length >= 1){
         for(int i = 0; i < length; i++){
             int min = i;
