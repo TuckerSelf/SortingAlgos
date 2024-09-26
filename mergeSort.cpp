35d5a073907691e94d8ada01a83340c8705eb723
Tucker Self <tself3@ut.utm.edu>
2024-09-25 19:25:07 -0500

    MergeSort

---
+++ mergeSort.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include "mergeSort.h"
+
+using namespace std;
+
+vector<int> separateVector(vector<int> &ar, int startVal, int endVal){
+    vector<int> output;
+    int startPos = 0;
+    while(ar[startPos] != startVal){
+        startPos++;
+    }
+    while(ar[startPos] != endVal){
+        output.push_back(ar[startPos])
+        startPos++;
+    }
+    return output;
+}
+
+void mergeSort::mergeSort(vector<int> &ar){
+    int frontVal = ar.front;
+    int backVal = ar.back;
+    if(frontVal < backVal){
+        int midVal = (frontVal + backVal)/2;
+        vector<int> frontHalf = separateVector(ar, frontVal, midVal);
+        vector<int> backHalf = separateVector(ar, midVal, backVal);
+        mergeSort(frontHalf);
+        mergeSort(backHalf);
+        int length = backHalf.size;
+        for(int i = 0; i < length; i++){
+            frontHalf.push_back(backHalf[i]);
+        }
+    }
+}
--- mergeSort.h
+++ mergeSort.h
@@ -5,9 +5,10 @@
 
 class mergeSort{
     private:
-
+        std::vector<int> separateVector(std::vector<int> &ar, int starVal, int endVal);
     public:
         void mergeSort(std::vector<int> &ar);
+
 }
 
 #endif //MERGESORT_H
